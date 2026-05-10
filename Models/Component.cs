69bba887b26d9539b2a628b6656e965cde2cdf2b
Maksymilian Golab <s33192@pjwstk.edu.pl>
2026-05-10 12:34:38 +0200

    Dodanie klas czesci

---
+++ Models/Component.cs
@@ -0,0 +1,15 @@
+﻿namespace ZadanieDomowe7.Models;
+
+public class Component
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public int ComponentManufacturersId { get; set; }
+    public int ComponentTypeId { get; set; }
+
+    public virtual ComponentManufacturer ComponentManufacturer { get; set; } = null!;
+    public virtual ComponentType ComponentType { get; set; } = null!;
+    public virtual ICollection<PCComponent> PCComponents { get; set; } = new List<PCComponent>();
+}
+
---
+++ Models/ComponentType.cs
@@ -0,0 +1,11 @@
+﻿namespace ZadanieDomowe7.Models;
+
+public class ComponentType
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Component> Components { get; set; } = new List<Component>();
+}
+
